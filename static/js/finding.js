6bf45d334e816594a8f711426b7145c5bf9b14de
jboegeholz <boegeholz.joern@gmail.com>
2017-08-12 21:43:55 +0200

    update

--- static/js/finding.js
+++ static/js/finding.js
@@ -4,29 +4,34 @@ function FindingViewModel() {
     self.findings = ko.observableArray();
 
     self.addFinding = function () {
-        console.log(self.findings().length);
-        self.findings.push({id: (self.findings().length + 1), trigger: ""});
+        //console.log(self.findings().length);
+        self.findings.push(new Finding(self.findings().length + 1, ""));
     };
     self.removeFinding = function () {
         self.findings.remove(this);
-        self.findings().foreach(function (entry) {
-            console.log(entry.id)
-        })
+        ko.utils.arrayForEach(self.findings(), function(value, i){
+            self.findings.replace(value, new Finding(i+1, value.trigger()));
+        });
     };
     self.update = function () {
         $.getJSON("/_get_findings", function (data) {
-            this.findings = data;
+            self.findings(data.findings);
         })
     };
 
 }
 
+function Finding(id, trigger){
+    var self = this;
+    self.id = ko.observable(id);
+    self.trigger = ko.observable(trigger);
+}
 
 ko.components.register('finding', {
     template: `<table>
             <tbody data-bind="foreach: findings">
             <tr>
-            <td><input data-bind="value: id"/></td>
+            <td><span data-bind="text: id"/></td>
             <td><input data-bind="value: trigger"/></td>
             <td><input /></td>
             <td><input /></td>
