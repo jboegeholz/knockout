ce7c4282f75edfd321b6ad1bce70f92a8b9ff788
jboegeholz <boegeholz.joern@gmail.com>
2017-08-18 22:18:16 +0200

    add options dynamically

--- static/js/tracePath.js
+++ static/js/tracePath.js
@@ -1,12 +1,14 @@
 class TracePathViewModel {
     constructor() {
         this.car = ko.observable();
+        this.availableCars = ko.observableArray(["Car_A", "Car_B", "Car_C"]);
         this.driver = ko.observable();
-        this.start_date = ko.observable();
-        this.end_date = ko.observable();
+        this.availableDriver = ko.observableArray(["Driver_A", "Driver_B"]);
+        this.start_date = ko.observable("01.01.2017");
+        this.end_date = ko.observable("02.01.2017");
 
         this.trace_path_proposal = ko.pureComputed(function () {
-            return this.car() + "_" + this.driver();
+            return this.car() + "_" + this.driver() + "_" + this.start_date() + "_" + this.end_date();
         }, this);
     }
 
@@ -18,10 +20,7 @@ ko.components.register('drive-details', {
             <div class="col">
             <div class="input-group">
             <span class="input-group-addon">Car</span>
-            <select class="form-control" data-bind="value: car">
-                <option>Fahrer A</option>
-                <option>Fahrer B</option>
-                <option>Fahrer C</option>
+            <select class="form-control" data-bind="options: availableCars, value: car">
             </select>
             </div>
             </div>
@@ -30,10 +29,7 @@ ko.components.register('drive-details', {
             <div class="col">
             <div class="input-group">
             <span class="input-group-addon">Driver</span>
-            <select class="form-control" data-bind="value: driver">
-                <option>Auto A</option>
-                <option>Auto B</option>
-                <option>Auto C</option>
+            <select class="form-control" data-bind="options: availableDriver, value: driver">
             </select></div></div></div>
                         <div class="row">
             <div class="col">
