b5707d48ac6b51f9a060e83bc2663fd24fa294f9
jboegeholz <boegeholz.joern@gmail.com>
2017-11-04 21:10:10 +0100

    add edit mode

--- static/js/installed_devices.js
+++ static/js/installed_devices.js
@@ -11,10 +11,18 @@ function DeviceViewModel() {
 
     self.installed_devices = ko.observableArray();
 
+    self.editMode = ko.observable(false);
+
 
     self.addDevice = function () {
         self.installed_devices.push(new Device(self.findings().length + 1, ""));
     };
+
+
+    self.editDevices = function () {
+        self.editMode(true);
+    };
+
 
     self.removeDevice = function (device) {
         self.installed_devices.remove(device);
@@ -36,13 +44,17 @@ function DeviceViewModel() {
 }
 
 ko.components.register('installed_devices', {
-    template: `<table class="table table-striped">
+    template: `<div data-bind="visible: editMode">
+    You will see this message only when "shouldShowMessage" holds a true value.
+</div>
+<table class="table table-striped">
             <thead class="thead-default">
              <tr>
                   <th>#</th>
                   <th>Name</th>
                   <th>Functional</th>
                   <th>Info</th>
+                  <th data-bind="visible: editMode">Remove</th>
                 </tr>
               </thead>
             <tbody data-bind="foreach: installed_devices">
@@ -56,11 +68,11 @@ ko.components.register('installed_devices', {
    <input type="radio" name="functional" value="false" data-bind="checked:functional"/>
 </label></td>
             <td><span data-bind="text: info"/></td>
-            <td  data-bind="click: $parent.removeFinding"><i class="glyphicon glyphicon-trash"></i></td>
+            <td  data-bind="visible: editMode, click: $parent.removeFinding"><i  data-bind="visible: editMode" class="glyphicon glyphicon-trash"></i></td>
             </tr></tbody></table>
             <div class="navbar navbar-inverse">
             <div class="navbar-inner">
-            <button class="navbar-btn pull-right" data-bind="click: addDeivce">Add a Finding</button>
+            <button class="navbar-btn pull-right" data-bind="click: editDevices">Edit</button>
             </div>
             </div>
             `,
